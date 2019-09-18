33ad21f87fbca22cb1799943406f02ae1f8d9c5e
Armine <https://github.com/ArmineKh>
2019-09-18 23:47:37 +0400

    added sending mail

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,19 +1,58 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use App\Page;
 use App\Service;
 use App\Portfolio;
 use App\People;
+use Validator;
+use Mail;
 use DB;
 
 class IndexController extends Controller
 {
     public function execute(Request $request){
+    	// POST Request
 
+    	if($request->isMethod('post')){
+    		// custom messages
+    		$messages = [
+    			'required' => 'Поле :attribute обязательно к заполнению',
+    			'email' => 'Поле :attribute должно соответствовать email адресу'
+    		];
+    		// $validatedData = $request->validate([
+    		// $this->validate($request,[
+    		Validator::make(
+    			[
+				'name' => $request->name,
+				'email' => $request->email,
+				'password' => $request->text,
+			],
+
+    			[
+    			'name' => 'required|max255',
+    			'email' => 'required|email',
+    			'text' => 'required'
+    		], $messages);
+    		$data = $request->all();
+
+    		//mail
+    		$result = Mail::send('site.email', ['data'=>$data], function($message) use ($data) {
+    			// $mail_admin = env('MAIL_ADMIN');
+    			$subject = 'Question';
+    			$message->from($data['email'], $data['name']);
+    			$message->to('arm.armin.armine@gmail.com', 'Mr. Admin')->subject($subject);
+    		});
+    		if($result){
+    			return redirect()->route('home')->with('status', 'Email is send');
+    		}
+
+    	}
+
+    	// GET Request
     	$pages = Page::all();
     	$portfolios = Portfolio::get(array('name', 'filter', 'images'));
     	$services = Service::where('id', '<' , 20)->get();
--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -1,10 +1,10 @@
 
-  @if(isset($pages) && is_object($pages))
+@if(isset($pages) && is_object($pages))
 
 @foreach($pages as $k=>$page)
 
-  @if($k%2 == 0)
-  <section id="home" class="top_cont_outer">
+@if($k%2 == 0)
+<section id="home" class="top_cont_outer">
   <div class="hero_wrapper">
     <div class="container">
       <div class="hero_section">
@@ -13,56 +13,56 @@
             <div class="top_left_cont zoomIn wow animated"> 
               {!! $page->text !!}
               <!-- <h2>We create <strong>awesome</strong> web templates</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text  printer took a galley of type and scrambled it to make a type specimen.</p> -->
-              <a href="{{route('page', array('alias'=>$page->alias))}}" class="read_more2">Read more</a> </div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text  printer took a galley of type and scrambled it to make a type specimen.</p> -->
+                <a href="{{route('page', array('alias'=>$page->alias))}}" class="read_more2">Read more</a> </div>
+              </div>
+              <div class="col-lg-7 col-sm-5">
+                {!! Html::image('assets/img/'.$page->images, 'a picture',  array('class' => 'zoomIn wow animated')) !!}
+                <!-- <img src="{{ asset('assets/img/main_device_image.png') }}" class="zoomIn wow animated" alt="" /> -->
+              </div>
+            </div>
           </div>
-          <div class="col-lg-7 col-sm-5">
-            {!! Html::image('assets/img/'.$page->images, 'a picture',  array('class' => 'zoomIn wow animated')) !!}
-      <!-- <img src="{{ asset('assets/img/main_device_image.png') }}" class="zoomIn wow animated" alt="" /> -->
-      </div>
         </div>
       </div>
-    </div>
-  </div>
-</section>
-<!--Hero_Section-->
-  @else
-<section id="aboutUs"><!--Aboutus-->
-<div class="inner_wrapper">
-  <div class="container">
-    <h2>{{$page->name}}</h2>
-    <div class="inner_section">
-  <div class="row">
-      <div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
-       {!! Html::image('assets/img/'.$page->images, 'a picture',  array('class' => 'img-circle delay-03s animated wow zoomIn')) !!}
-  
+    </section>
+    <!--Hero_Section-->
+    @else
+    <section id="aboutUs"><!--Aboutus-->
+      <div class="inner_wrapper">
+        <div class="container">
+          <h2>{{$page->name}}</h2>
+          <div class="inner_section">
+            <div class="row">
+              <div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
+               {!! Html::image('assets/img/'.$page->images, 'a picture',  array('class' => 'img-circle delay-03s animated wow zoomIn')) !!}
 
-        <!-- <img src="{{ asset('assets/img/about-img.jpg') }}" class="img-circle delay-03s animated wow zoomIn" alt=""> -->
 
-      </div>
-        <div class=" col-lg-7 col-md-7 col-sm-7 col-xs-12 pull-left">
-          <div class=" delay-01s animated fadeInDown wow animated">
-          {!! $page->text !!}
+               <!-- <img src="{{ asset('assets/img/about-img.jpg') }}" class="img-circle delay-03s animated wow zoomIn" alt=""> -->
+
+             </div>
+             <div class=" col-lg-7 col-md-7 col-sm-7 col-xs-12 pull-left">
+              <div class=" delay-01s animated fadeInDown wow animated">
+                {!! $page->text !!}
 
       <!-- <h3>Lorem Ipsum has been the industry's standard dummy text ever..</h3><br/> 
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.PageMaker including versions of Lorem Ipsum.</p> <br/>
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged like Aldus PageMaker including versions of Lorem Ipsum.</p> -->
-</div>
-<div class="work_bottom"> <span>Want to know more..</span> <a href="{{route('page', array('alias'=>$page->alias))}}" class="contact_btn">Contact Us</a> </div>       
-     </div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged like Aldus PageMaker including versions of Lorem Ipsum.</p> -->
+          </div>
+          <div class="work_bottom"> <span>Want to know more..</span> <a href="{{route('page', array('alias'=>$page->alias))}}" class="contact_btn">Contact Us</a> </div>       
+        </div>
         
       </div>
-    
+
       
     </div>
   </div> 
-  </div>
+</div>
 </section>
 <!--Aboutus--> 
-  @endif
+@endif
 @endforeach
 
-  @endif
+@endif
 
 @if(isset($services) && is_object($services))
 
@@ -71,13 +71,13 @@
   <div class="container">
     <h2>Services</h2>
     <div class="service_wrapper">
-    
-    @foreach($services as $k=>$service)
-    @if($k == 0 || $k%3 == 0)
+
+      @foreach($services as $k=>$service)
+      @if($k == 0 || $k%3 == 0)
       <div class="row {{($k != 0) ? 'borderTop' : ''}}">
-  
-    @endif
-    <div class="col-lg-4 {{ ($k % 3 > 0) ? 'borderLeft' : ''}} {{($k>2) ? 'mrgTop' : ''}}">
+
+        @endif
+        <div class="col-lg-4 {{ ($k % 3 > 0) ? 'borderLeft' : ''}} {{($k>2) ? 'mrgTop' : ''}}">
           <div class="service_block">
             <div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa {{$service->icon}}"></i></span> </div>
             <h3 class="animated fadeInUp wow">{{$service->name}}</h3>
@@ -87,9 +87,9 @@
 
         @if(($k + 1)%3 == 0)
       </div>
-        @endif
+      @endif
 
-    @endforeach
+      @endforeach
 <!-- 
       <div class="row">
         
@@ -142,10 +142,10 @@
 
 <!-- Portfolio -->
 <section id="Portfolio" class="content"> 
-  
+
   <!-- Container -->
   <div class="container portfolio_title"> 
-    
+
     <!-- Title -->
     <div class="section-title">
       <h2>Portfolio</h2>
@@ -164,13 +164,13 @@
       <ul class="clearfix">
         <li><a id="all" href="#" data-filter="*" class="active">
           <h5>All</h5>
-          </a></li>
-          @foreach($tags as $tag)
+        </a></li>
+        @foreach($tags as $tag)
 
         <li><a class="" href="#" data-filter=".{{$tag}}">
           <h5>{{$tag}}</h5>
-          </a></li>
-          @endforeach
+        </a></li>
+        @endforeach
 
         <!-- <li><a class="" href="#" data-filter=".design">
           <h5>Design</h5>
@@ -183,7 +183,7 @@
           </a></li>
         <li><a class="" href="#" data-filter=".web">
           <h5>Web App</h5>
-          </a></li> -->
+        </a></li> -->
       </ul>
     </div>
     @endif
@@ -198,15 +198,15 @@
       <!-- Portfolio Item -->
       <div style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four   {{$item->filter}} isotope-item">
         <div class="portfolio_img">
-        {{Html::image('assets/img/'.$item->images, '$item->name')}}
-         <!-- <img src="{{ asset('assets/img/portfolio_pic1.jpg') }}"  alt="Portfolio 1"> -->
-          </div>        
+          {{Html::image('assets/img/'.$item->images, '$item->name')}}
+          <!-- <img src="{{ asset('assets/img/portfolio_pic1.jpg') }}"  alt="Portfolio 1"> -->
+        </div>        
         <div class="item_overlay">
           <div class="item_info"> 
             <h4 class="project_name">{{$item->name}}</h4>
           </div>
         </div>
-        </div>
+      </div>
 
       @endforeach
       
@@ -273,7 +273,7 @@
           </div>
         </div>
         </a> </div>
-       -->
+      -->
     </div>
     <!--/Portfolio Wrapper --> 
     
@@ -287,34 +287,26 @@
     <div class="clear"></div>
     <div id="project_data"></div>
   </div>
- 
-  
+
 </section>
 <!--/Portfolio --> 
 
 @endif
 
 
-
-
-
-
-
-
-
 <section class="page_section" id="clients"><!--page_section-->
   <h2>Clients</h2>
-<!--page_section-->
-<div class="client_logos"><!--client_logos-->
-  <div class="container">
-    <ul class="fadeInRight animated wow">
-      <li><a href="javascript:void(0)"><img src="{{ asset('assets/img/client_logo1.png') }}" alt=""></a></li>
-      <li><a href="javascript:void(0)"><img src="{{ asset('assets/img/client_logo2.png') }}" alt=""></a></li>
-      <li><a href="javascript:void(0)"><img src="{{ asset('assets/img/client_logo3.png') }}" alt=""></a></li>
-      <li><a href="javascript:void(0)"><img src="{{ asset('assets/img/client_logo4.png') }}" alt=""></a></li>
-    </ul>
+  <!--page_section-->
+  <div class="client_logos"><!--client_logos-->
+    <div class="container">
+      <ul class="fadeInRight animated wow">
+        <li><a href="javascript:void(0)"><img src="{{ asset('assets/img/client_logo1.png') }}" alt=""></a></li>
+        <li><a href="javascript:void(0)"><img src="{{ asset('assets/img/client_logo2.png') }}" alt=""></a></li>
+        <li><a href="javascript:void(0)"><img src="{{ asset('assets/img/client_logo3.png') }}" alt=""></a></li>
+        <li><a href="javascript:void(0)"><img src="{{ asset('assets/img/client_logo4.png') }}" alt=""></a></li>
+      </ul>
+    </div>
   </div>
-</div>
 </section>
 <!--client_logos-->
 
@@ -325,12 +317,16 @@
     <h2>Team</h2>
     <h6>Lorem ipsum dolor sit amet, consectetur adipiscing.</h6>
     <div class="team_section clearfix">
-    @foreach($peoples as $people)
+
+      @foreach($peoples as $k=>$people)
 
       <div class="team_area">
-        <div class="team_box wow fadeInDown delay-03s">
+        <div class="team_box wow fadeInDown delay-0{{($k*3 + 3)}}s">
           <div class="team_box_shadow"><a href="javascript:void(0)"></a></div>
-          <img src="{{ asset('assets/img/team_pic1.jpg') }}" alt="">
+
+          {{Html::image('assets/img/'.$people->emages, '$people->name')}}
+
+          <!-- <img src="{{ asset('assets/img/team_pic1.jpg') }}" alt=""> -->
           <ul>
             <li><a href="javascript:void(0)" class="fa fa-twitter"></a></li>
             <li><a href="javascript:void(0)" class="fa fa-facebook"></a></li>
@@ -338,12 +334,12 @@
             <li><a href="javascript:void(0)" class="fa fa-google-plus"></a></li>
           </ul>
         </div>
-        <h3 class="wow fadeInDown delay-03s">Tom Rensed</h3>
-        <span class="wow fadeInDown delay-03s">Chief Executive Officer</span>
-        <p class="wow fadeInDown delay-03s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat sollicitudin cursus. Dolor sit amet, consectetur adipiscing elit proin consequat.</p>
+        <h3 class="wow fadeInDown delay-0{{($k*3 + 3)}}s">{{$people->name}}</h3>
+        <span class="wow fadeInDown delay-0{{($k*3 + 3)}}s">{{$people->position}}</span>
+        <p class="wow fadeInDown delay-0{{($k*3 + 3)}}s">{{$people->text}}</p>
       </div>
 
-    @endforeach
+      @endforeach
 
 
 <!-- 
@@ -396,54 +392,65 @@
         <h2>Contact Us</h2>
         <div class="row">
           <div class="col-lg-4">
-            
+
           </div>
           <div class="col-lg-4">
-           
+
           </div>
           <div class="col-lg-4">
-          
+
           </div>
         </div>
       </div>
       <div class="row">
         <div class="col-lg-4 wow fadeInLeft">	
-		 <div class="contact_info">
-                            <div class="detail">
-                                <h4>UNIQUE Infoway</h4>
-                                <p>104, Some street, NewYork, USA</p>
-                            </div>
-                            <div class="detail">
-                                <h4>call us</h4>
-                                <p>+1 234 567890</p>
-                            </div>
-                            <div class="detail">
-                                <h4>Email us</h4>
-                                <p>support@sitename.com</p>
-                            </div> 
-                        </div>
-       		  
-			  
-          
-          <ul class="social_links">
-            <li class="twitter animated bounceIn wow delay-02s"><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
-            <li class="facebook animated bounceIn wow delay-03s"><a href="javascript:void(0)"><i class="fa fa-facebook"></i></a></li>
-            <li class="pinterest animated bounceIn wow delay-04s"><a href="javascript:void(0)"><i class="fa fa-pinterest"></i></a></li>
-            <li class="gplus animated bounceIn wow delay-05s"><a href="javascript:void(0)"><i class="fa fa-google-plus"></i></a></li> 
-          </ul>
-        </div>
-        <div class="col-lg-8 wow fadeInLeft delay-06s">
-          <div class="form">
-            <input class="input-text" type="text" name="" value="Your Name *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
-            <input class="input-text" type="text" name="" value="Your E-mail *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
-            <textarea class="input-text text-area" cols="0" rows="0" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">Your Message *</textarea>
-            <input class="input-btn" type="submit" value="send message">
+         <div class="contact_info">
+          <div class="detail">
+            <h4>UNIQUE Infoway</h4>
+            <p>104, Some street, NewYork, USA</p>
           </div>
+          <div class="detail">
+            <h4>call us</h4>
+            <p>+1 234 567890</p>
+          </div>
+          <div class="detail">
+            <h4>Email us</h4>
+            <p>support@sitename.com</p>
+          </div> 
+        </div>
+
+
+
+        <ul class="social_links">
+          <li class="twitter animated bounceIn wow delay-02s"><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+          <li class="facebook animated bounceIn wow delay-03s"><a href="javascript:void(0)"><i class="fa fa-facebook"></i></a></li>
+          <li class="pinterest animated bounceIn wow delay-04s"><a href="javascript:void(0)"><i class="fa fa-pinterest"></i></a></li>
+          <li class="gplus animated bounceIn wow delay-05s"><a href="javascript:void(0)"><i class="fa fa-google-plus"></i></a></li> 
+        </ul>
+      </div>
+      <div class="col-lg-8 wow fadeInLeft delay-06s">
+        <div class="form">
+
+          <form action="{{ route('home')}}" method="post">
+
+          <input class="input-text" type="text" name="name" value="Your Name *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
+          <input class="input-text" type="text" name="email" value="Your E-mail *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
+          <textarea class="input-text text-area" name="text" cols="0" rows="0" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">Your Message *</textarea>
+          <input class="input-btn" type="submit" value="send message">
+
+          <!-- <input  type="hidden" name="_token" value="{{csrf_token()}}"> -->
+          {{csrf_field()}}
+
+
+          
+          
+          </form>
         </div>
       </div>
-    </section>
-  </div>
-  <div class="container">
-    <div class="footer_bottom"><span>Copyright © 2014,    Template by <a href="http://webthemez.com">WebThemez.com</a>. </span> </div>
-  </div>
+    </div>
+  </section>
+</div>
+<div class="container">
+  <div class="footer_bottom"><span>Copyright © 2014,    Template by <a href="http://webthemez.com">WebThemez.com</a>. </span> </div>
+</div>
 </footer>
